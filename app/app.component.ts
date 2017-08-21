def65d79f7f48e405f6ffe2944139b740b18b1e5
unknown <vdsc@udel.edu>
2017-08-21 16:15:35 -0400

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  loadedFeature ='recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
+ }
+
+
+
+
+
+
+
---
+++ app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient} from '../shared/ingredient.model';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [new Ingredient("apple", 5), new Ingredient("tomatoes", 6)];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
